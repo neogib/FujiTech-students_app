4ec47e447ee1118e96108ad435af12677b094b19
Jakub Brambor <kuba.brambor@gmail.com>
2025-02-28 21:18:01 +0100

    add page

---
+++ frontend/pages/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+// Dodajemy importy z nuxt
+import { useRuntimeConfig, useFetch } from "nuxt/app";
+
+interface BackendResponse {
+  message: string;
+}
+
+const config = useRuntimeConfig();
+console.log("API Base:", config.public.apiBase);
+const { data, error, pending } = useFetch<BackendResponse>(`/api/users`, {
+  onRequest({ request, options }) {
+    console.log("Starting Request:", request);
+  },
+  onResponse({ request, response, options }) {
+    console.log("Response:", response._data);
+  },
+  onRequestError({ request, error, options }) {
+    console.log("Request Error:", error);
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h1>Test połączenia z backendem</h1>
+    <div v-if="pending">Ładowanie...</div>
+    <div v-else-if="error">Błąd: {{ error }}</div>
+    <div v-else>
+      <p>Odpowiedź z backendu: {{ data?.message }}</p>
+    </div>
+  </div>
+</template>
