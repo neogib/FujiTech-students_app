18aae143181a82a0e01a13da5f62d97c5ddc1d13
Jakub Brambor <kuba.brambor@gmail.com>
2025-02-28 17:34:58 +0100

    add cors handling for frontend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# List of allowed origins
+origins = [
+    # default port for Nuxt.js
+    "http://localhost:3000",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
