5455aea40b63ef6de1bbad4e3df60bd50f72ee0d
Jakub Brambor <kuba.brambor@gmail.com>
2025-02-28 21:17:43 +0100

    add frontend

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+
+  // Runtime environment configuration
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:8000",
+    },
+  },
+
+  routeRules: {
+    "/api/**": {
+      proxy: "http://localhost:8000/**",
+    },
+  },
+});
