9763baab44cf9cb02bfe19b8376a28b3b3eba56e
Jakub Brambor <kuba.brambor@gmail.com>
2025-03-13 19:58:56 +0100

    fix review issues

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
 import { useApi } from "../composables/useApi";
-import { onMounted, watch, watchEffect } from "vue";
 
 interface BackendResponse {
   message: string;
 }
 
-const { data, error, pending } = useApi<BackendResponse>("/users");
+const { data, error, status } = useApi<BackendResponse>("/users");
 </script>
 
 <template>
   <div>
     <h1>Test połączenia z backendem</h1>
-    <div v-if="pending">Ładowanie...</div>
+    <div v-if="status === 'pending'">Ładowanie...</div>
     <div v-else-if="error">Błąd: {{ error }}</div>
     <div v-else>
       <p>Odpowiedź z backendu: {{ data?.message }}</p>
