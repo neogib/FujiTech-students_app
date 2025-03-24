34c56ae969e946a8beea8cb1d43e1281410beec1
Gibirizon <swojteksz@gmail.com>
2025-03-24 22:21:46 +0100

    fix: seperate szkoly base and table model - created seperate classes `SzkolyBase` and `Szkoly` for the response models in FastAPI - set email as EmailStr type, update dependencies with `email-validator` - set max_length for all str columns to limit the length of VARCHAR type - added missing __init__.py file

--- backend/app/models/schools.py
+++ backend/app/models/schools.py
@@ -1,21 +1,30 @@
 from geoalchemy2 import Geometry
+from pydantic import EmailStr
 from sqlmodel import Column, Field, SQLModel
 
 
-class Szkoly(SQLModel):
-    id: int | None = Field(default=None, primary_key=True)
+class SzkolyBase(SQLModel):
     numer_rspo: int = Field(unique=True, index=True)
     nip: str
     regon: str
     liczba_uczniow: int
-    nazwa: str = Field(index=True)
-    nazwa_skrocona: str = Field(index=True)
-    dyrektor_imie: str | None = None
-    dyrektor_nazwisko: str | None = None
+    nazwa: str = Field(index=True, max_length=150)
+    dyrektor_imie: str | None = Field(default=None, max_length=50)
+    dyrektor_nazwisko: str | None = Field(default=None, max_length=50)
     geolokalizacja: Geometry = Field(
         sa_column=Column(Geometry(geometry_type="POINT", srid=4326))
     )
     numer_budynku: int
-    telefon: str | None = None
-    email: str | None = None
-    strona_internetowa: str | None = None
+    telefon: str | None = Field(
+        default=None,
+        max_length=15,  # E.164 standard allows up to 15 digits
+    )
+    email: EmailStr | None = Field(
+        default=None,
+        max_length=254,  # RFC 3696 official limit
+    )
+    strona_internetowa: str | None = Field(default=None, max_length=254)
+
+
+class Szkoly(SzkolyBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
