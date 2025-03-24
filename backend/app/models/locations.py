ef1db29ba47617fc17df217de08216cdeea70ecd
Gibirizon <swojteksz@gmail.com>
2025-03-24 23:24:45 +0100

    feat: implement locations tables - created all base and table models for all 5 kind of locations - connected locations with "one to one" relationship with schools table by id - created additional relations using "Realtionship Attributes"

---
+++ backend/app/models/locations.py
@@ -0,0 +1,54 @@
+from sqlmodel import Field, Relationship, SQLModel
+
+from .schools import Szkoly
+
+
+class WojewodztwaBase(SQLModel):
+    nazwa: str = Field(index=True)
+    teryt: int = Field(index=True)
+
+
+class Wojewodztwa(WojewodztwaBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    szkola: Szkoly | None = Relationship(back_populates="wojewodztwo")
+
+
+class PowiatyBase(SQLModel):
+    nazwa: str = Field(index=True)
+    teryt: int = Field(index=True)
+
+
+class Powiaty(PowiatyBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    szkola: Szkoly | None = Relationship(back_populates="powiat")
+
+
+class GminyBase(SQLModel):
+    nazwa: str = Field(index=True)
+    teryt: int = Field(index=True)
+
+
+class Gminy(GminyBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    szkola: Szkoly | None = Relationship(back_populates="gmina")
+
+
+class MiejscowosciBase(SQLModel):
+    nazwa: str = Field(index=True)
+    teryt: int = Field(index=True)
+    kod_pocztowy: str
+
+
+class Miejscowosci(MiejscowosciBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    szkola: Szkoly | None = Relationship(back_populates="miejscowosc")
+
+
+class UliceBase(SQLModel):
+    nazwa: str = Field(index=True)
+    teryt: int = Field(index=True)
+
+
+class Ulice(UliceBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    szkola: Szkoly | None = Relationship(back_populates="ulica")
--- backend/app/models/schools.py
+++ backend/app/models/schools.py
@@ -1,6 +1,8 @@
 from geoalchemy2 import Geometry
 from pydantic import EmailStr
-from sqlmodel import Column, Field, SQLModel
+from sqlmodel import Column, Field, Relationship, SQLModel
+
+from .locations import Gminy, Miejscowosci, Powiaty, Ulice, Wojewodztwa
 
 
 class SzkolyBase(SQLModel):
@@ -25,6 +27,20 @@ class SzkolyBase(SQLModel):
     )
     strona_internetowa: str | None = Field(default=None, max_length=254)
 
+    # Foreign keys
+    wojewodztwo_id: int | None = Field(default=None, foreign_key="wojewodztwa.id")
+    powiat_id: int | None = Field(default=None, foreign_key="powiaty.id")
+    gmina_id: int | None = Field(default=None, foreign_key="gminy.id")
+    miejscowosc_id: int | None = Field(default=None, foreign_key="miejscowosci.id")
+    ulica_id: int | None = Field(default=None, foreign_key="ulice.id")
+
 
 class Szkoly(SzkolyBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
+
+    # Relationships
+    wojewodztwo: Wojewodztwa | None = Relationship(back_populates="szkola")
+    powiat: Powiaty | None = Relationship(back_populates="szkola")
+    gmina: Gminy | None = Relationship(back_populates="szkola")
+    miejscowosc: Miejscowosci | None = Relationship(back_populates="szkola")
+    ulica: Ulice | None = Relationship(back_populates="szkola")
