c027fd74dafcb6d56c8e27027aeddc0952beeb0b
Gibirizon <swojteksz@gmail.com>
2025-03-28 15:59:37 +0100

    feat: implement relations between locations

--- backend/app/models/locations.py
+++ backend/app/models/locations.py
@@ -8,37 +8,46 @@ if TYPE_CHECKING:
 
 class WojewodztwaBase(SQLModel):
     nazwa: str = Field(index=True)
-    teryt: int = Field(index=True)
+    teryt: str = Field(index=True)
 
 
 class Wojewodztwa(WojewodztwaBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
+
     szkola: list["Szkoly"] = Relationship(back_populates="wojewodztwo")
+    powiaty: list["Powiaty"] = Relationship(back_populates="wojewodztwo")
 
 
 class PowiatyBase(SQLModel):
     nazwa: str = Field(index=True)
-    teryt: int = Field(index=True)
+    teryt: str = Field(index=True)
+    wojewodztwo_id: int = Field(foreign_key="wojewodztwa.id")
 
 
 class Powiaty(PowiatyBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
+
     szkola: list["Szkoly"] = Relationship(back_populates="powiat")
+    wojewodztwo: Wojewodztwa = Relationship(back_populates="powiaty")
+    gminy: list["Gminy"] = Relationship(back_populates="powiat")
 
 
 class GminyBase(SQLModel):
     nazwa: str = Field(index=True)
-    teryt: int = Field(index=True)
+    teryt: str = Field(index=True)
+    powiat_id: int = Field(foreign_key="powiaty.id")
 
 
 class Gminy(GminyBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
+
     szkola: list["Szkoly"] = Relationship(back_populates="gmina")
+    powiat: Powiaty = Relationship(back_populates="gminy")
 
 
 class MiejscowosciBase(SQLModel):
     nazwa: str = Field(index=True)
-    teryt: int = Field(index=True)
+    teryt: str = Field(index=True)
     kod_pocztowy: str
 
 
@@ -49,7 +58,7 @@ class Miejscowosci(MiejscowosciBase, table=True):
 
 class UliceBase(SQLModel):
     nazwa: str = Field(index=True)
-    teryt: int = Field(index=True)
+    teryt: str = Field(index=True)
 
 
 class Ulice(UliceBase, table=True):
