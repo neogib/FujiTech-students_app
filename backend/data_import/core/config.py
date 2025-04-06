3681e721868c10747bf2ba40749f100f4af9134b
Gibirizon <swojteksz@gmail.com>
2025-04-06 08:40:48 +0200

    fix: set starting page back to 1 by default

--- backend/data_import/core/config.py
+++ backend/data_import/core/config.py
@@ -1,10 +1,8 @@
 class APISettings:
     API_SCHOOLS_URL: str = "https://api-rspo.men.gov.pl/api/placowki/"
     HEADERS: dict[str, str] = {"accept": "application/ld+json"}
-    START_PAGE: int = 211
-    PAGE_LIMIT: int | None = (
-        None  # the last page to fetch, later this can be changed or set to None
-    )
+    START_PAGE: int = 1
+    PAGE_LIMIT: int | None = None  # the last page to fetch, if None there is no limit
     MAX_SCHOOLS_SEGMENT: int = 1000
 
 
