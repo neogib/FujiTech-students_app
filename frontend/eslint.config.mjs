d497f95c9fafdeea69ed11a39f84eecee6e7739b
Gibirizon <swojteksz@gmail.com>
2025-07-15 23:00:25 +0200

    feat: setup prettier with eslint-config-prettier

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default withNuxt({
 	rules: {
 		"no-else-return": 2,
 	},
-});
+}).append(eslintConfigPrettier);
