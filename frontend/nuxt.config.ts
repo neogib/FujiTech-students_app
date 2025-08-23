a70fc0931a45843504fe72df9ea8fe25cfcb6ffa
Jakub Brambor <kuba.brambor@gmail.com>
2025-08-23 14:27:19 +0200

    fix nuxt config error

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,7 @@
+import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: "2024-11-01",
+    compatibilityDate: "2025-08-23",
     devtools: { enabled: true },
 
     // Runtime environment configuration
