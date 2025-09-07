799036522595a66801154f724c57bebb815e48f0
Jakub Brambor <kuba.brambor@gmail.com>
2025-09-07 20:09:36 +0200

    add ignore rules

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,6 +3,18 @@ import withNuxt from "./.nuxt/eslint.config.mjs";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default withNuxt({
+    ignores: [
+        "**/node_modules/**",
+        "**/.nuxt/**",
+        "**/.output/**",
+        "**/dist/**",
+        "**/coverage/**",
+        "**/.husky/**",
+        "**/.vscode/**",
+        "**/.idea/**",
+        "**/.DS_Store",
+        "**/pnpm-lock.yaml",
+    ],
     rules: {
         "no-else-return": 2,
     },
