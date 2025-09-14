d3746310591570a206a4f05bdbcf1803fc9c2c1f
Gibirizon <swojteksz@gmail.com>
2025-09-14 15:01:53 +0200

    feat: implement transparent navbar on map page

--- frontend/components/nav/NavBar.vue
+++ frontend/components/nav/NavBar.vue
@@ -1,5 +1,11 @@
 <template>
-    <nav class="sticky top-0 z-50 bg-white shadow-lg">
+    <nav
+        class="top-0 z-50"
+        :class="
+            transparent
+                ? 'fixed w-full bg-white/80'
+                : 'sticky bg-white shadow-lg'
+        ">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between items-center h-16">
                 <!-- Logo/Brand -->
@@ -63,7 +69,7 @@
                 leave-active-class="transition duration-150 ease-in"
                 leave-from-class="transform scale-100 opacity-100"
                 leave-to-class="transform scale-95 opacity-0">
-                <div v-if="isMobileMenuOpen" class="md:hidden bg-white">
+                <div v-if="isMobileMenuOpen" class="md:hidden">
                     <div
                         class="px-2 pt-2 pb-3 space-y-1"
                         @click="closeMobileMenu">
@@ -76,7 +82,14 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue"
+interface Props {
+    transparent?: boolean
+}
+
+// directly destructuring props to eliminate unused variable
+const { transparent } = withDefaults(defineProps<Props>(), {
+    transparent: false,
+})
 
 const isMobileMenuOpen = ref(false)
 
--- frontend/pages/map.vue
+++ frontend/pages/map.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <NavBar />
+        <NavBar transparent />
 
         <!-- MapView taking full remaining space (this component will only be rendered on client side) -->
         <MapView />
